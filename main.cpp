d40fe7796d439062c6e55ffe189a9fdb03390fca
Peter Stedman <peterfstedman@gmail.com>
2021-03-08 11:38:35 -0500

    Added menu options that call appropriate functions

--- main.cpp
+++ main.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 
 enum AppState {Running, Done, Error};
 
-//Todo - Shows options
-void Menu() {
-    std::cout << "Ran menu" << std::endl;
-}
-
 //Todo - shows completed or current tasks to command line
-void ShowTasks(char category) {
+void ShowTasks(AppState &editor_state) {
     std::cout << "Showed Tasks" << std::endl;
 }
 
 //Todo - adds a task to Tasks.txt
-void AddTask() {
+void AddTask(AppState &editor_state) {
     std::cout << "Added Task" << std::endl;
 }
 
 //Todo - adds a task to Completed.txt
-void CompleteTask() {
+void CompleteTask(AppState &editor_state) {
     std::cout << "Completed Task" << std::endl;
+}
+
+//Todo - Deletes a task from Tasks.txt
+void RemoveTask(AppState &editor_state) {
+    std::cout << "Removed Task" << std::endl;
 }
 
 // run again if the user wishes
@@ -37,15 +38,59 @@ void EndCheck(AppState &editor_state) {
     case 'n':
         editor_state = Done;
         break;
+    /*
+     * I think this might cause memory problems if the user keeps entering the wrong
+     * value - maybe a do/while or while true loop might be better
+    */
     default:
         EndCheck(editor_state);
         break;
     }
 }
+
+void Menu(AppState &editor_state) {
+    std::cout << "Options:" << std::endl;
+    std::cout << std::setw(8) << "" << "1. Show Tasks" << std::endl;
+    std::cout << std::setw(8) << "" << "2. Add Task" << std::endl;
+    std::cout << std::setw(8) << "" << "3. Complete Task" << std::endl;
+    std::cout << std::setw(8) << "" << "4. Remove Task" << std::endl;
+    std::cout << "Enter number of choice: ";
+    
+    char menu_choice = '0';
+
+    std::cin >> menu_choice;
+
+    switch (menu_choice)
+    {
+    case '1':
+        ShowTasks(editor_state);
+        break;
+
+    case '2':
+        AddTask(editor_state);
+        break;
+    case '3':
+        CompleteTask(editor_state);
+        break;
+    case '4':
+        RemoveTask(editor_state);
+        break;
+    
+    default:
+        editor_state = Error;
+        break;
+    }
+}
+
 int main() {
     AppState editor = Running;
     while (editor == Running) {
-        Menu();
+        // check for error before running
+        if (editor == Error) {
+            std::cout << "Error while running" << std::endl;
+            return 1;
+        }
+        Menu(editor);
         EndCheck(editor);
     }
     return 0;
